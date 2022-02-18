6c6f695ed2ac32e26ab179028b27b54cc85e1560
Aleksandra <a.haniok@gmail.com>
2022-02-18 14:36:14 +0000

    Add the solved challenge

---
+++ rest-parameter-syntax-challenge.js
@@ -0,0 +1,25 @@
+/*
+You will assign an array to a variable which will contain a copy of the array mixedLetters and 6 other letters.
+You will create an arrow function which should take two parameters. This function should combine all the argument 
+passed in into a single array and sort them and reverse them and then return this array
+You are free to choose an arrow function of your choice to do this.
+*/
+
+let mixedLetters = ['b', 'd', 'a', 'c', 'f', 'e'];
+
+let moreMixedLetters = [...mixedLetters, 'h', 'k', 'g', 'j', 'i', 'l'];
+
+console.log(moreMixedLetters);
+
+// start
+const updateSortReverse = (arr, ...letters) => {
+    let allLetters = arr;
+    for (let i of letters) {
+        allLetters.push(i);
+    }
+    return allLetters.sort().reverse();
+}
+
+let reverseSort = updateSortReverse(moreMixedLetters, 'n', 'm', 'o')
+console.log(reverseSort);
+console.log(mixedLetters);
